98634c7fb7380444d204524acc6eb36b2e95864f
Jesse Jacobsen <jessej9876@gmail.com>
2018-11-06 15:07:28 -0500

    added LED
    Added an Led on pin 20, it turns on loop

--- finalproject.ino
+++ finalproject.ino
@@ -1,9 +1,11 @@
+int ledPin= 20
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, HIGH);
 }
